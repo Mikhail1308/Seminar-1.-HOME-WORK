5db5908d773f4ea4aafcdf4d8731569424ddf1a4
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-15 01:56:12 +0300

    add Creat TASK_02 Определение максимума из трёх введенных чисел

---
+++ TASK_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая определяет максимум из трёх введенных  чисел
+Console.WriteLine("Введите число= ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число= ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число= ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if ((numberB < numberA) && (numberA > numberC))
+{
+    int numberMax = numberA;
+    Console.WriteLine($"Максимальное число= {numberA}");
+}
+
+else if ((numberA < numberB) && (numberB > numberC))
+{
+    int numberMax = numberB;
+    Console.WriteLine($"Максимальное число= {numberB}");
+}
+else
+{
+    int numberMax = numberC;
+    Console.WriteLine($"Максимальное число= {numberC}");
+}
