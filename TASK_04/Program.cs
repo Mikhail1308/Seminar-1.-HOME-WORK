166f677204ae280083aa54db0f68e13004c61625
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-15 15:19:56 +0300

    add Create TASK_04 Показать все четные числа в диапазоне от 1 до вводимого числа N

---
+++ TASK_04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа показывает все четные числа в диапазоне от 1 до вводимого числа N
+Console.WriteLine("Введите длинну диапазона чисел = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Четные числа в заданном диапазоне -> ");
+var results = new List<int>();
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        results.Add(i);
+    }
+}
+Console.WriteLine(string.Join(", ", results));
+Console.WriteLine("Введите Enter для завершения");
+Console.ReadLine();
