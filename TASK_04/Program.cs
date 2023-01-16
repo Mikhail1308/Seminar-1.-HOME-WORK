14c9af25b90ccda2fbff24223eb20c3d133976b8
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-16 20:13:43 +0300

    add Added new solutions

--- TASK_04/Program.cs
+++ TASK_04/Program.cs
@@ -2,14 +2,45 @@
 Console.WriteLine("Введите длинну диапазона чисел = ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.Write("Четные числа в заданном диапазоне -> ");
-var results = new List<int>();
-for (int i = 1; i <= N; i++)
+for (int i = 1; i <= N; i = i + 1)
+if (i % 2 == 0)
 {
-    if (i % 2 == 0)
-    {
-        results.Add(i);
-    }
+    Console.Write(i + "  ");
 }
-Console.WriteLine(string.Join(", ", results));
 Console.WriteLine("Введите Enter для завершения");
 Console.ReadLine();
+
+
+
+
+// Программа показывает все четные числа в диапазоне от 1 до вводимого числа N
+// Console.WriteLine("Введите длинну диапазона чисел = ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Четные числа в заданном диапазоне -> ");
+// var results = new List<int>();
+// for (int i = 1; i <= N; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         results.Add(i);
+//     }
+// }
+// Console.WriteLine(string.Join(", ", results));
+// Console.WriteLine("Введите Enter для завершения");
+// Console.ReadLine();
+
+
+// Veriant 3
+// Console.Write("Write number 1: ");
+// int n = int.Parse(Console.ReadLine());
+// Console.Write("Write number 2: ");
+// int m = int.Parse(Console.ReadLine());
+// for (int i = n; i <= m; i++)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
+// Console.WriteLine("Введите Enter для завершения");
+// Console.ReadLine();
